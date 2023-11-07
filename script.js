b86664ad6e1a82b9f3fb71ed070483e5ddc026b6
yongava <>
2023-11-07 16:37:45 +0700

    update

--- script.js
+++ script.js
@@ -13,50 +13,38 @@ function setupGame() {
   for (let i = 1; i <= 4; i++) { // Include Room 4 for watcher
     let roomDiv = document.getElementById('room' + i);
     roomDiv.addEventListener('click', function() {
-      revealWords(i);
+      if (!gameState.roomClicked) {
+        if (i === 4) {
+          revealAllWordsForWatcher();
+        } else {
+          revealOtherWordsForPlayer(i);
+        }
+        gameState.roomClicked = true; // Prevent any further clicks
+      }
     });
   }
 }
 
-function revealWords(roomNumber) {
-  // Check if any room has already been clicked
-  if (gameState.roomClicked) return;
+function revealOtherWordsForPlayer(roomNumber) {
+  // Find the room and reveal other players' words
+  let roomDiv = document.getElementById('room' + roomNumber);
+  let room = gameState.rooms['room' + roomNumber];
+  let otherWords = Object.values(gameState.rooms)
+    .filter((_, index) => index !== roomNumber - 1)
+    .map(room => room.playerWord);
 
-  // If it's the watcher's room (Room 4), reveal all rooms
-  if (roomNumber === 4) {
-    for (let i = 1; i <= 3; i++) {
-      revealPlayerWords(i);
-    }
-  } else {
-    // Otherwise, reveal only the clicked room's word
-    revealPlayerWords(roomNumber);
-  }
-
-  // Update the game state to reflect that a room has been clicked
-  gameState.roomClicked = true;
+  roomDiv.innerHTML = '<strong>Room ' + roomNumber + ' for Player</strong>';
+  roomDiv.innerHTML += '<p>See Other Words: ' + otherWords.join(', ') + '</p>';
 }
 
-function revealPlayerWords(roomNumber) {
-  // Find the room and change its state to revealed
-  let roomKey = 'room' + roomNumber;
-  gameState.rooms[roomKey].revealed = true;
-
-  // Change the content of the div to reveal the words
-  let roomDiv = document.getElementById(roomKey);
-  let room = gameState.rooms[roomKey];
-  if (roomNumber !== 4) {
-    roomDiv.innerHTML = '<strong>Room ' + roomNumber + ' for Player</strong>';
-    roomDiv.innerHTML += '<p>Your Word: ' + room.playerWord + '</p>';
-    roomDiv.innerHTML += '<p>Other Words: ' + room.otherWords.join(', ') + '</p>';
-  } else {
-    // For watcher room, reveal all player words
-    roomDiv.innerHTML = '<strong>Room 4 for Watcher</strong>';
-    Object.keys(gameState.rooms).forEach(roomKey => {
-      let playerRoom = gameState.rooms[roomKey];
-      roomDiv.innerHTML += `<p>Room ${roomKey.charAt(roomKey.length - 1)} Word: ` +
-                           `${playerRoom.playerWord}</p>`;
-    });
-  }
+function revealAllWordsForWatcher() {
+  // Watcher room reveals all words
+  let watcherDiv = document.getElementById('room4');
+  watcherDiv.innerHTML = '<strong>Room 4 for Watcher</strong>';
+  Object.keys(gameState.rooms).forEach(roomKey => {
+    let room = gameState.rooms[roomKey];
+    watcherDiv.innerHTML += `<p>Room ${roomKey.charAt(roomKey.length - 1)} Word: ${room.playerWord}</p>`;
+  });
 }
 
 // Initialize the game
