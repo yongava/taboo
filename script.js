95419967ab37a0356a784b014d4044914f72b5d9
yongava <>
2023-11-13 15:22:04 +0700

    new ux

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ let gameState = {
 
 function setupGame() {
   // Add click listeners to each player's room
-  for (let i = 1; i <= 4; i++) { // Include Room 4 for watcher
+  for (let i = 1; i <= 4; i++) {
     let roomDiv = document.getElementById('room' + i);
     roomDiv.addEventListener('click', function() {
       if (!gameState.roomClicked) {
@@ -24,46 +24,59 @@ function setupGame() {
         } else {
           revealOtherWordsForPlayer(i);
         }
-        gameState.roomClicked = true; // Prevent any further clicks
+        gameState.roomClicked = true;
       }
     });
   }
 }
 
 function revealOtherWordsForPlayer(roomNumber) {
-  // Find the room and reveal other players' words along with their names
-  let roomDiv = document.getElementById('room' + roomNumber);
-  let otherRooms = Object.entries(gameState.rooms)
-    .filter(([key, _]) => key !== 'room' + roomNumber)
-    .map(([key, room]) => {
-      let playerName = gameState.roomNames[key];
-      return `${playerName}'s word: ${room.playerWord}`;
-    });
+  // Hide other rooms and center the selected room
+  for (let i = 1; i <= 4; i++) {
+    let roomDiv = document.getElementById('room' + i);
+    if (i !== roomNumber) {
+      roomDiv.style.display = 'none';
+    } else {
+      roomDiv.style.width = '100%';
+      roomDiv.style.textAlign = 'center';
+      let room = gameState.rooms['room' + roomNumber];
+      let otherRooms = Object.entries(gameState.rooms)
+        .filter(([key, _]) => key !== 'room' + roomNumber)
+        .map(([key, room]) => {
+          let playerName = gameState.roomNames[key];
+          return `${playerName}'s word: ${room.playerWord}`;
+        });
 
-  roomDiv.innerHTML = '<strong>' + gameState.roomNames['room' + roomNumber] + "'s Room</strong>";
-  roomDiv.innerHTML += '<p>See Other Words: ' + otherRooms.join(', ') + '</p>';
+      roomDiv.innerHTML = '<strong>' + gameState.roomNames['room' + roomNumber] + "'s Room</strong>";
+      roomDiv.innerHTML += '<p>See Other Words: ' + otherRooms.join(', ') + '</p>';
+    }
+  }
 }
 
-
 function revealAllWordsForWatcher() {
-  // Watcher room reveals all words with player names
+  // Hide player rooms and only show the watcher's room
+  for (let i = 1; i <= 3; i++) {
+    let roomDiv = document.getElementById('room' + i);
+    roomDiv.style.display = 'none';
+  }
+
   let watcherDiv = document.getElementById('room4');
+  watcherDiv.style.width = '100%';
+  watcherDiv.style.textAlign = 'center';
   watcherDiv.innerHTML = '<strong>Room 4 for Watcher</strong>';
   Object.keys(gameState.rooms).forEach(roomKey => {
-    let room = gameState.rooms[roomKey];
     let playerName = gameState.roomNames[roomKey];
+    let room = gameState.rooms[roomKey];
     watcherDiv.innerHTML += `<p>${playerName}'s Word: ${room.playerWord}</p>`;
   });
 }
 
-
-// Call this function at the end of setupGame to set the names
 function setRoomNames() {
   for (let i = 1; i <= 3; i++) {
     document.getElementById('name' + i).textContent = gameState.roomNames['room' + i] + "'s";
   }
 }
 
-// Call this at the end of the setupGame function
+// Initialize the game and set the room names
 setupGame();
 setRoomNames();
